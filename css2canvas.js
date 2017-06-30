b7a0d7fe0d6dcd12feac9b1f4cc9105a23965fa6
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-06-30 17:52:00 +0800

    Add html2canvas
    render dom to canvas

--- css2canvas.js
+++ css2canvas.js
@@ -1,17 +1,48 @@
 var css2canvas = (function(){
 
-	this.canvas = null ;
+	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+								window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+	var documentBody = document.body ;
+
+	var props = {
+		canvas : null ,
+		ctx : null ,
+		requestID : null ,
+		DOM : null ,
+		animation : null ,  
+		x : 1 
+	}
 
 	function init(canvas,config){
-		this.canvas = canvas ;
+		props.canvas = canvas ;
+		props.ctx = props.canvas.getContext('2d') ;
 	}
 
 	function draw(){
+		documentBody.appendChild(props.DOM) ;
+		html2canvas(props.DOM, {
+			onrendered: function(domCanvas) {
+				documentBody.removeChild(props.DOM);
+				props.canvas.width = props.canvas.width ;
+				props.ctx.drawImage(domCanvas,props.x ++  ,0);
+				requestID = requestAnimationFrame(draw) ;
+			}
+		});
+	}
+
+	function addDOM(dom){
+		props.DOM = dom ;
+	}
+
+	function addAnimation(){
 
 	}
 
 	return {
-		init : init
-		draw : draw 
+		init : init ,
+		draw : draw ,
+		addDOM : addDOM ,
+		addAnimation : addAnimation
 	}
 })();
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var block = document.createElement('div') ;
 document.body.appendChild(block);
 var style = {
-	width : 100 ,
-	heigth : 100,
-	background : 'red' ,
-	transition : 'width 2s' 
+	width : '100px' ,
+	heigth : '100px' ,
+	backgroundColor : 'red' ,
+  animationDuration : 2
 }
+var animation = {
+  0 : { 
+	  marginLeft : '0px'  
+	} ,
+  100 : { 
+	  marginLeft : '300px' 
+	}
+}
+
 var canvas = document.createElement('canvas') ;
+canvas.width = 500 ;
 document.body.appendChild(canvas); 
 
+var dom = document.createElement('div') ;
+for ( var key in style ){
+	dom.style[key] = style[key] ;
+}
 css2canvas.init(canvas);
-css2canvas.draw(style) ;
+css2canvas.addDOM(dom) ;
+css2canvas.addAnimation(animation) ;
+css2canvas.draw();
